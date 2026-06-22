56e4b56249bcb9178d93134f98282e545086bb55
heavenlol <heavenlol@users.noreply.github.com>
2026-06-22 17:15:27 -0500

    Switch to imguimc as dependency

--- src/main/java/dev/imguitest/ImGuiTestMod.java
+++ src/main/java/dev/imguitest/ImGuiTestMod.java
@@ -1,35 +1,20 @@
 package dev.imguitest;
 
+import foundry.imguimc.event.ImGuiSetupEvent;
+import foundry.imguimc.event.RenderImGuiEvent;
 import imgui.ImGui;
-import imgui.ImGuiIO;
-import imgui.app.Application;
-import imgui.app.Configuration;
-import imgui.flag.ImGuiConfigFlags;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.HudElementRegistry;
-import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 
 public class ImGuiTestMod implements ClientModInitializer {
-
-    private static boolean imguiInitialized = false;
-
     @Override
     public void onInitializeClient() {
-        HudRenderCallback.EVENT.register((drawContext, tickDeltaManager) -> {
-            if (!imguiInitialized) {
-                ImGui.createContext();
-                ImGuiIO io = ImGui.getIO();
-                io.addConfigFlags(ImGuiConfigFlags.NavEnableKeyboard);
-                imguiInitialized = true;
-            }
-            ImGui.newFrame();
+        RenderImGuiEvent.START.register(() -> {
             ImGui.begin("ImGuiTest");
-            ImGui.text("Hello from ImGuiTest mod on 26.2!");
+            ImGui.text("Hello from ImGuiTest on 26.2!");
             if (ImGui.button("Click Me")) {
                 System.out.println("[ImGuiTest] Button clicked!");
             }
             ImGui.end();
-            ImGui.render();
         });
     }
 }
