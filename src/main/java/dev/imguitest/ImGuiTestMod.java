9ed7be937fc52186fbb6be4bf46854409c2606ef
heavenlol <heavenlol@users.noreply.github.com>
2026-06-22 17:22:15 -0500

    Fix build and mod class with correct event API

--- src/main/java/dev/imguitest/ImGuiTestMod.java
+++ src/main/java/dev/imguitest/ImGuiTestMod.java
@@ -1,14 +1,13 @@
 package dev.imguitest;
 
-import foundry.imguimc.event.ImGuiSetupEvent;
-import foundry.imguimc.event.RenderImGuiEvent;
+import foundry.imgui.fabric.api.event.RenderImGuiEventsFabric;
 import imgui.ImGui;
 import net.fabricmc.api.ClientModInitializer;
 
 public class ImGuiTestMod implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        RenderImGuiEvent.START.register(() -> {
+        RenderImGuiEventsFabric.PRE.register(() -> {
             ImGui.begin("ImGuiTest");
             ImGui.text("Hello from ImGuiTest on 26.2!");
             if (ImGui.button("Click Me")) {
